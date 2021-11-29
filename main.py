79a89892ffac78462e9044b400e9b167fb406622
Maximus Sallam <maximus.sallam@gmail.com>
2021-11-29 15:35:31 +0800

    Part 4.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import ntpath
+import ntpath as path
 import pygame as pg
 import sys
+import tilemap
 from settings import *
 from sprites import *
+from tilemap import *
 
 
 class Game:
@@ -15,22 +17,20 @@ class Game:
         self.load_data()
 
     def load_data(self):
-        game_folder = ntpath.dirname(__file__)
-        self.map_data = []
-        with open(ntpath.join(game_folder, "map.txt"), "rt") as f:
-            for line in f:
-                self.map_data.append(line)
+        game_folder = path.dirname(__file__)
+        self.map = Map(path.join(game_folder, "map.txt"))
 
     def new(self):
         # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
-        for row, tiles in enumerate(self.map_data):
+        for row, tiles in enumerate(self.map.data):
             for col, tile in enumerate(tiles):
                 if tile == "1":
                     Wall(self, col, row)
                 if tile == "P":
                     self.player = Player(self, col, row)
+        self.camera = Camera(self.map.width, self.map.height)
 
     def run(self):
         # game loop - set self.playing = False to end the game
@@ -48,6 +48,7 @@ class Game:
     def update(self):
         # update portion of the game loop
         self.all_sprites.update()
+        self.camera.update(self.player)
 
     def draw_grid(self):
         for x in range(0, WIDTH, TILESIZE):
@@ -58,7 +59,8 @@ class Game:
     def draw(self):
         self.screen.fill(BGCOLOR)
         self.draw_grid()
-        self.all_sprites.draw(self.screen)
+        for sprite in self.all_sprites:
+            self.screen.blit(sprite.image, self.camera.apply(sprite))
         pg.display.flip()
 
     def events(self):
